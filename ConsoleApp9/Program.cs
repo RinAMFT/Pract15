61faf3bf405c555c7d5ec86efab62cb8eab20832
RinAMFT <mucurkaarina@gmail.com>
2024-12-25 09:20:25 +0200

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Func()
+        {
+            int count = 0;
+            Random random = new Random();
+            for (int x = 0; x <= 15;x++)
+            {
+                int number = random.Next(-20, 30);
+                Console.WriteLine(number);
+                if (number >0)
+                    count++;
+            }
+            Console.WriteLine($"count={ count}");
+        }
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+    }
+}
