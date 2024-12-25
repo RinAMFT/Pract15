b925276bb41a3068a385a80e5c19ad9204fa3724
RinAMFT <mucurkaarina@gmail.com>
2024-12-25 09:14:23 +0200

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Func()
+        {
+            Random random = new Random();
+            for(int x = 0; x<=10; x++)
+            {
+                int number = random.Next(-40, 40);
+                Console.WriteLine(number);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+    }
+}
