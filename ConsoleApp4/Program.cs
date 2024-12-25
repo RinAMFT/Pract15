2861df493e201437074d3096bf7c233cba3e9193
RinAMFT <mucurkaarina@gmail.com>
2024-12-25 08:47:22 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Func()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("20  ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+    }
+}
