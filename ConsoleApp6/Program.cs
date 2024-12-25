e7ac9de9a777aa10db41b3e88bd3846cbf144d41
RinAMFT <mucurkaarina@gmail.com>
2024-12-25 09:09:15 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Func()
+        {
+            int count = 0;
+            for (int x = 10; x <= 99; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    count++;
+                    Console.WriteLine(x);
+                }
+
+            }
+            Console.WriteLine($"count={count}");
+        }
+            static void Main(string[] args)
+            {
+                Func();
+                Console.ReadKey();
+            }
+        
+    }
+}
