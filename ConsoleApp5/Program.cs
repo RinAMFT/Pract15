35ea2393d5e82b0bd7b4487960a0e6dfeed286de
RinAMFT <mucurkaarina@gmail.com>
2024-12-25 08:54:40 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Func()
+        {
+            double price = 20.4;
+            for (int i = 2; i <= 20; i++)
+            {
+                Console.WriteLine($"{i}шт.={price * i} рубли");
+            }
+        }
+            static void Main(string[] args)
+        {
+                Func(); 
+                Console.ReadKey();
+        }
+    }
+}
